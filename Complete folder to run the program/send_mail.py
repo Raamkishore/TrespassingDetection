76bc6300abd2f65cb5c828bce912e9b5a53dca96
Raamkishore <raamkishore.p@gmail.com>
2020-03-07 23:17:05 +0530

    Add files via upload

---
+++ Complete folder to run the program/send_mail.py
@@ -0,0 +1,56 @@
+import smtplib
+import time
+import datetime
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d, %H:%M:%S')
+PASSWORD = "bbbqgfvkvpavrrwb" # This is the gmail App Password
+OUR_MAIL = "trespassing.detection@gmail.com"
+SUBJECT = "Trespasser Detected!"
+MESSAGE = ("Hi!\n\nA trespasser has been detected in your area at " + st + ". "
+    "You can find an image with the tresspasser in the attachments!"
+    "\n\nStay safe,\nTresspassing Detection Team")
+
+
+def send_mail(send_to, file):
+    port = 465  # For SSL
+    
+    # Create a secure SSL context
+    context = smtplib.ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        # Log in with the App Password
+        server.ehlo() 
+        server.login(OUR_MAIL, PASSWORD)
+
+        msg = MIMEMultipart()
+        msg['From'] = OUR_MAIL
+        msg['To'] = send_to
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = SUBJECT
+
+        msg.attach(MIMEText(MESSAGE))
+
+        with open(file, "rb") as f:
+            part = MIMEApplication(
+                f.read(),
+                Name=basename(file)
+            )
+            print("File " + file + " read successfully")
+
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(file)
+        msg.attach(part)
+
+        print("Sending mail...")
+        server.sendmail(OUR_MAIL, send_to, msg.as_string())
+        server.close()
+        print("Mail sent successfully")
+
+def main():
+    send_mail("give_your_mail_id_here", "trespassers.png")
